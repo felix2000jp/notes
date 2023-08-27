05dd0e6e0eaebf3cad0d600b3e393769946d1b28
felix2000jp <felix2000jp@gmail.com>
2023-08-27 19:38:18 +0100

    fixed client base url

--- client/src/utils/http-client.ts
+++ client/src/utils/http-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { handleError } from "./problem-details";
 
 export const httpClient = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://localhost:8080/api/",
   withCredentials: true
 });
 
