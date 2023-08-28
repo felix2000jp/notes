e1bee9fe9359686c6db6b827e03ad309681ed6c1
felix2000jp <felix2000jp@gmail.com>
2023-08-28 10:37:12 +0100

    containerized the entire app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,10 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: { port: 5070 },
+  server: {
+    host: true,
+    port: 80
+  },
   resolve: { alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }] },
   plugins: [react()]
 });
